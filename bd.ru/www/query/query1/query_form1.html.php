a4fa3f45c1e23e24ac0921582de63dc516438d0e
Vlad Khakin <xottab404@gmail.com>
2014-11-09 15:43:40 +0400

    nothing cnanges

--- bd.ru/www/query/query1/query_form1.html.php
+++ bd.ru/www/query/query1/query_form1.html.php
@@ -9,8 +9,8 @@
 			 <hr>
             <h3 align=center> Введите период времени <h3>
 				<form action="" method="get" align=center>
-                <input type="text" value="20.01.2013" name="date1" />
-                <input type="text" value="20.02.2013" name="date2"/>
+                <input type="date"  name="date1" />
+                <input type="date"  name="date2"/>
                 <input type="submit" value="Подтвердить" name="button" />
                 </form>
 </body>
